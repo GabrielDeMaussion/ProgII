416773146d1d837163ebdcc512f16ecaf670d6a9
gabydemaussion <gabydemaussion@hotmail.com>
2023-08-21 17:48:45 -0300

    primer commit

---
+++ Clase_15.08 (CarpinteriaApp)/Entidades/DetallePresupuesto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_15._08__CarpinteriaApp_.Entidades
+{
+    internal class DetallePresupuesto
+    {
+        //Atributos
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+
+        
+        //Metodos
+
+        //Calcular subtotal
+        public double CalcularSubtotal()
+        {
+            double total = 0;
+            total = Producto.Precio * Cantidad;
+            return total;
+        }
+    }
+}
