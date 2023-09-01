046e5274cb023c2d202d5f2e292ce8025c80ae3b
Gabriel <gabydemaussion@hotmail.com>
2023-09-01 18:25:36 -0300

    Inicio proyecto veterinaria

---
+++ VeterinariaApp/Entidades/Cliente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeterinariaApp.Entidades
+{
+    internal class Cliente
+    {
+        //Atributos
+        string nombre;
+        string sexo;
+        int codigo;
+        List<Mascota> mascotas;
+
+
+        //Propiedades
+        public string Nombre            { get {  return nombre; }       set {  nombre = value; } }
+        public string Sexo              { get {  return sexo; }         set {  sexo = value; } }
+        public int Codigo               { get {  return codigo; }       set {  codigo = value; } }
+        public List<Mascota> Mascotas   { get { return mascotas; }      set {  mascotas = value; } }
+
+
+        //Constructor
+        public Cliente(string Nombre = "(Sin especificar)", string Sexo = "(Sin especificar)", int Codigo = -1, List<Mascota> Mascotas = null)
+        {
+            this.Nombre = Nombre;
+            this.Sexo = Sexo;
+            this.Codigo = Codigo;
+
+            //Creacion de la lista vacia en caso de "null"
+            if (Mascotas != null)
+            {
+                this.Mascotas = Mascotas;
+            }
+            else
+            {
+                this.Mascotas = new List<Mascota>();
+            }
+
+        }
+    }
+}
---
+++ VeterinariaApp/Entidades/Mascota.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeterinariaApp.Entidades
+{
+    internal class Mascota
+    {
+        //Atributos
+        internal string nombre;
+        internal int edad;
+        internal string tipo;
+
+
+        //Propiedades
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public int Edad { get { return edad; } set {  edad = value; } }
+        public string Tipo { get { return tipo; } set { tipo = value; } }
+
+
+        //Constructor
+        public Mascota(string Nombre = "(Sin especificar)", int Edad = -1, string Tipo = "(Sin especificar)") 
+        { 
+            this.Nombre = Nombre;
+            this.Edad = Edad;
+            this.Tipo = Tipo;
+        }
+
+    }
+}
